f64c63090cdd790558218153a46610227eda3b2e
Livingsaint2 <ofukowoicho2@gmail.com>
2022-12-18 08:22:55 -0800

    100 print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Print combinations of two digit numbers
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: A program that prints all possible different combination of two
+ * Numbers must to separated by , followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * print only the smallest combination of two digits
+ * Return: 0
  */
 int main(void)
 {
-	int tens;
-	int ones;
+	int c;
+	int d = 0;
 
-	 for (tens = 0; tens <= 9; tens++)
-	 {
-		 for (ones = tens + 1; ones <= 9; ones++)
-		 {
-			 putchar(tens + '0');
-			 putchar(ones + '0');
-			 if (tens < 8)
-			 {
-				 putchar(';');
-				 putchar(' ');
-			 }
-		 }
-	 }
-	 putchar('\n');
-	 return (0);
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar('.');
+				}
+			}
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
 }

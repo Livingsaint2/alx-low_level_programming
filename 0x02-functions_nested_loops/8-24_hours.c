d6e65bca1987999804deaf80856e0ff55e824071
Livingsaint2 <ofukowoicho2@gmail.com>
2022-12-21 12:24:04 -0800

    8 to 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -30,7 +30,7 @@ void jack_bauer(void)
 				{
 					_putchar('0' + h2);
 					_putchar('0' + h1);
-					_putchar(';');
+					_putchar(':');
 					_putchar('0' + m2);
 					_putchar('0' + m1);
 					_putchar('\n');
